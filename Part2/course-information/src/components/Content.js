17b5f2bfd83eb681989c614c1f0d3a7b5daf50f6
Buket Karakaş <buketkarakas8@gmail.com>
2021-03-30 01:13:36 +0300

    Implement 2.1 exercise

---
+++ Part2/course-information/src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ parts }) => {
+    
+    return (
+      <>
+        {parts.map(
+            (part) => <Part part = {part} />
+        )}
+      </>
+    )
+  }
+
+export default Content;
