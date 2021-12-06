b6b308e33cd68494449f83dd232198309ca16acf
Buket <buketkarakas8@gmail.com>
2021-12-06 14:57:16 +0200

    Implementation of 4.7

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -35,9 +35,30 @@ const mostBlogs = (blogs) => {
     return authorArray[0]
 }
 
+const mostLikes = (blogs) => {
+    const authorsObj = _.reduce(blogs, function(result, blog){
+        if (result[blog.author]){
+            result[blog.author] += blog.likes
+        }
+        else
+            result[blog.author] = 0 + blog.likes
+        return result
+    },{})
+    let authorArray = []
+    _.forIn(authorsObj, function(value, key){
+        authorArray=authorArray.concat({
+            author: key,
+            likes: value
+        })
+    })
+    authorArray.sort((author1,author2) => author2.likes - author1.likes)
+    return authorArray[0]
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
