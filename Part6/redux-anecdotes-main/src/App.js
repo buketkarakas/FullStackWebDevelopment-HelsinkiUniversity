a5d46b7498cf1c8572e1104f4676c671256fa367
Buket <buketkarakas8@gmail.com>
2022-01-03 17:59:24 +0200

    Implementation of 6.6

--- Part6/redux-anecdotes-main/src/App.js
+++ Part6/redux-anecdotes-main/src/App.js
@@ -1,34 +1,21 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { createAnecdote, increaseVotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    dispatch({
-      type: "VOTE",
-       data: {id: id}
-      })
+    dispatch(increaseVotes(id))
   }
-
-  const getId = () => (100000 * Math.random()).toFixed(0)
-
 
   const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdoteContent.value
     event.target.anecdoteContent.value = ''
-    dispatch(
-      {
-        type: "NEW_ANECDOTE",
-        data: {
-          content,
-          id: getId,
-          votes: 0
-        }
-      }
-    )
+    dispatch(createAnecdote(content))
+     
   }
 
   return (
